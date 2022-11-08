2428bdaaf5e7afca55e56049ed597a422a283ab6
Benjamin Nomba <bnomba0704@gmail.com>
2022-11-08 11:33:49 -0800

    new ui

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -14,9 +14,10 @@ const Favorites = ({OnDelete, cont, fav, display, nam, OnSearch, searchMovie, se
         </Helmet>
 
         <div className="heading"><Heading cont={cont} nam={nam} OnSearch={OnSearch} searchMovie={searchMovie} setSearchMovie={setSearchMovie}/></div>
-        <div className={`container-2 ${cont === 'container-watch-list' && 'container-2-style'}`}>
+        <div className={`container-2 container-2-style}`}>
+            <div className='space-div'></div>
             <h2>Watch List</h2>
-            <div className="row movie">
+            <div className="fav-style row movie">
               <h2 className={display}> There are no movies in Watch List </h2>
               <MovieList title='Favourites' OnDelete={OnDelete} movies={fav}/>
             </div>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import LazyLoad from 'react-lazy-load';
 
 const MovieList = ({movies, OnTap, OnDelete, title, loading}) => {
 
@@ -11,7 +12,9 @@ const MovieList = ({movies, OnTap, OnDelete, title, loading}) => {
       {(movies).map((movie, index) =>(
 
           <div className="wrapper">
-            <img style={{height:'300px'}} key={index} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="movie" />
+            <LazyLoad threshold={0.95}>
+              <img style={{height:'300px'}} key={index} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="movie" />
+            </LazyLoad>
             {movie.poster_path !== null ?<button onClick={ title !== 'Movies' ?() => OnDelete(movie) :() => OnTap(movie) } className={`btn ${ title !== 'Movies' ? 'delete' : 'button'}`}>{ title === 'Movies' ? 'Add to Watch List' : 'DELETE'}</button> : ''}
           </div>
                  
