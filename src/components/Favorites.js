185acce6023108d8e47fbd964a915a17fc304cc3
Benjamin Nomba <bnomba0704@gmail.com>
2023-09-24 04:22:07 -0700

    style

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -10,7 +10,7 @@ const Favorites = ({OnDelete, cont, fav, display, nam, OnSearch, searchMovie, se
     <div className={`container-2 ${cont === 'container-watch-list' && 'container-2-style'}`}>
         <h2>Watch List</h2>
         <div className="row movie">
-          <h2 className={display}> There are no movies in Watch List </h2>
+          <h2 className={`width ${display}`}> There are no movies in Watch List </h2>
           <MovieList title='Favourites' OnDelete={OnDelete} movies={fav}/>
         </div>
     </div>
